a801f7106ab7ae36bd2aaca1e17096401b1c40dd
Ashton <ashton.sickler@icloud.com>
2024-07-29 01:42:32 -0500

    Implimented lexer

--- main.cpp
+++ main.cpp
@@ -1,5 +1,41 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
 
-int main() {
-    std::cout << "hello world" << std::endl;
+#include "src/lexer.hpp"
+
+int main(int argc, char* argv[]) {
+    // Check if a filename was provided
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <filename>\n";
+        exit(1);
+    }
+
+    std::string filename = argv[1];
+    std::ifstream file(filename);
+
+    // Check if the file was opened successfully
+    if (!file.is_open()) {
+        std::cerr << "Error: Could not open file " << filename << "\n";
+        exit(1);
+    }
+
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    std::string contents = buffer.str();
+    contents += " ";
+
+    // Close the file
+    file.close();
+
+    Lexer lex = Lexer(contents);
+    auto tokens = lex.tokenize();
+    for (Token token : tokens) {
+        std::cout << token.value << " -> " << token.kind << std::endl;
+    }
+
+
+    return 0;
 }
---
+++ src/lexer.hpp
@@ -0,0 +1,179 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+enum TokenKind {
+
+    Eof,
+
+    // Multi char tokens
+    Ident,
+    Number,
+    QuotedString,
+
+    RightArrow,
+    LeftArrow,
+
+    GreaterThan,
+    LessThan,
+
+    And,
+    Or,
+    PlusPlus,
+    MinusMinus,
+    DoubleEquals,
+
+    // Single char tokens
+    OpenParen,
+    CloseParen,
+    OpenBrack,
+    CloseBrack,
+    OpenCurly,
+    CloseCurly,
+
+    Tilda,
+    Bang,
+    Question,
+    Pipe,
+
+    BinOperator,
+};
+
+class Token {
+    public:
+    std::string value;
+    TokenKind kind;
+
+    Token(std::string v, TokenKind k) : value(v), kind(k) {}
+    Token(char v, TokenKind k) : value(std::string(1, v)), kind(k) {}
+};
+
+class Lexer {
+    public:
+    std::string source;
+    int index;
+
+    Lexer(std::string source) : source(source) {}
+
+    std::vector<Token> tokenize() {
+        std::vector<Token> tokens;
+        std::string buf;
+
+        while (source[index] != '\0') {
+            // Handle multi char tokens
+            // TODO: Expand to recognizing
+            if (source[index] == ' ' || source[index] == '\n' || source[index] == '\t') {
+                if (isalpha(buf[0])) {
+                    tokens.push_back(Token(buf, Ident));
+                }
+                if (isdigit(buf[0])) {
+                    int dot_found = 0;
+                    for (char c : buf) {
+                        if (!isdigit(c) || dot_found > 1) {
+                            std::cerr << "Cannot have alpha characters in number : " << buf << std::endl;
+                            exit(1);
+                        }
+                    }
+                    tokens.push_back(Token(buf, Number));
+                }
+                buf = "";
+                index++;
+                continue;
+            }
+
+            if (isalnum(source[index])) {
+                buf += source[index];
+            }
+
+            if (source[index] == '/' && source[index + 1] == '/') {
+                while (source[index] != '\n') {
+                    index++;
+                }
+            }
+
+            else if (source[index] == '/' && source[index + 1 == '*']) {
+                index += 2;
+                while (source[index] != '*' && source[index + 1] != '/') {
+                    index++;
+                }
+                index += 2;
+            }
+
+            if (source[index] == '"') {
+                std::string comment_buf;
+                do {
+                    comment_buf += source[index];
+                    index++;
+                } while (source[index] != '"');
+                comment_buf += '"';
+                tokens.push_back(Token(comment_buf, QuotedString));
+                index++;
+            }
+            else if (source[index] == '\'') {
+                std::string comment_buf;
+                do {
+                    comment_buf += source[index];
+                    index++;
+                } while (source[index] != '\'');
+                comment_buf += '\'';
+                tokens.push_back(Token(comment_buf, QuotedString));
+                index++;
+            }
+
+            if (source[index] == '-' && source[index + 1] == '>') {
+                tokens.push_back(Token("->", RightArrow));
+                index += 1;
+            }
+            else if (source[index] == '<' && source[index + 1] == '-') {
+                tokens.push_back(Token("<-", LeftArrow));
+                index += 2;
+            }
+
+            if (source[index] == '=' && source[index + 1] == '=') {
+                tokens.push_back(Token("==", DoubleEquals));
+                index += 1;
+            }
+            else if (source[index] == '+' && source[index + 1] == '+') {
+                tokens.push_back(Token("++", PlusPlus));
+                index += 2;
+            }
+            else if (source[index] == '-' && source[index + 1] == '-') {
+                tokens.push_back(Token("--", MinusMinus));
+                index += 2;
+            }
+
+            switch (source[index]) {
+                case '{':
+                    tokens.push_back(Token(source[index], OpenCurly));
+                    break;
+                case '}':
+                    tokens.push_back(Token(source[index], CloseCurly));
+                    break;
+                case '[':
+                    tokens.push_back(Token(source[index], OpenBrack));
+                    break;
+                case ']':
+                    tokens.push_back(Token(source[index], CloseBrack));
+                    break;
+                case '(':
+                    tokens.push_back(Token(source[index], OpenParen));
+                    break;
+                case ')':
+                    tokens.push_back(Token(source[index], CloseParen));
+                    break;
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                    tokens.push_back(Token(source[index], BinOperator));
+                    break;
+            }
+            index++;
+        }
+
+        tokens.push_back(Token("EOF", Eof));
+
+        return tokens;
+    }
+
+};
